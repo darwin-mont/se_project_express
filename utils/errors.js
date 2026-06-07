3d5fda41820398ad242cfe7d642d4ebbbe0fd93f
darwin-mont <darwinm95@live.com>
2026-06-07 09:56:55 -0400

    4th commit - project 12

--- utils/errors.js
+++ utils/errors.js
@@ -1,14 +1,19 @@
 const { SOME_ERROR_CODE } = require("../utils/errors");
 
 const createUser = (req, res) => {
-  User.create(...)    // arguments omitted
-    .then(...)        // handle successful request
+  User.create(req.body)
+    .then((user) => res.status(201).send(user))
     .catch((err) => {
       console.error(err);
-      if (err.name === 'SomeErrorName') {
-        return res.status(SOME_ERROR_CODE).send({ message: "Appropriate error message" })
+      if (err.name === "SomeErrorName") {
+        return res
+          .status(SOME_ERROR_CODE)
+          .send({ message: "Appropriate error message" });
       } else {
         // if no errors match, return a response with status code 500
+        return res.status(500).send({ message: "Internal server error" });
       }
     });
-}
+};
+
+exports.createUser = createUser;
