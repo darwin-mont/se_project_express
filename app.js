b3953f518761c5ade747b4e96198ce093f8e9f45
darwin-mont <darwinm95@live.com>
2026-06-07 10:56:09 -0400

    6th commit : project 12
    changed port from 3000 to 3001 for git actions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
 
 const app = express();
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db").then(() => {
   console.log("Connected to MongoDB");
